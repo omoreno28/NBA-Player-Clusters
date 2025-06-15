4b9559e16246bdcbfbd64f1cbab7f837bdd4a58d
Orion Moreno <129994194+omoreno28@users.noreply.github.com>
2025-06-15 13:40:29 -0700

    Update NBA_player_clusters.py

--- NBA_player_clusters.py
+++ NBA_player_clusters.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import StandardScaler
 import numpy as np
 
 # Load Data
-data = pd.read_csv('/Users/orionmoreno/Streamlit Projects/NBA Similar Players Project/NBA_PLAYERS.csv')
+data = pd.read_csv('https://raw.githubusercontent.com/omoreno28/NBA-Player-Clusters/main/NBA_PLAYERS.csv')
 
 # Filter relevant numeric columns for clustering
 features = ['PTS', 'TRB', 'AST', 'FG%', 'FG3%', 'FT%', 'eFG%', 'PER', 'WS']
